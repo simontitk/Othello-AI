7bac0dbd5e273dfefbfe5a817c35afe3d140beb1
simontitk <simontitk@gmail.com>
2024-03-14 21:17:16 +0100

    separate minvalue-maxvalue logic

--- src/HooliganAI.java
+++ src/HooliganAI.java
@@ -5,33 +5,67 @@ public class HooliganAI  implements IOthelloAI {
     private BoardEvaluator evaluator;
     private int MAX_DEPTH;
 
+    private static class MoveValue {
+        private Position move;
+        private int value;
+        private MoveValue(Position move, int value) {
+            this.move = move;
+            this.value = value;
+        }
+    }
+
     public HooliganAI() {
         this.evaluator = new BoardEvaluator();
-        this.MAX_DEPTH = 1;
+        this.MAX_DEPTH = 5;
     }
+
 
     @Override
     public Position decideMove(GameState s) {
-        int depth = 1;
-        int playerToMove = s.getPlayerInTurn();
-        Position bestMove = new Position(-1, -1);
-        ArrayList<Position> moves = s.legalMoves();
-        int startValue = Integer.MIN_VALUE;
-        
-        for (var move: moves) {
-            int[][] currentBoard = s.getBoard();
-            GameState newState = new GameState(currentBoard, playerToMove);
-            newState.insertToken(move);
-            if (depth == this.MAX_DEPTH) {
-                int value = this.evaluator.evaluate(newState);
-                if (value >= startValue) {
-                    startValue = value;
-                    bestMove = move;
-                }
-            }             
-        }
-
-        return bestMove;
+        int depth = 0;
+        MoveValue mv = (s.getPlayerInTurn() == 1) ? this.maxValue(s, depth) : this.minValue(s, depth);
+        return mv.move;
     }
-    
+
+
+    private MoveValue maxValue(GameState s, int depth) {
+        MoveValue mv = new MoveValue(new Position(-1, -1), Integer.MIN_VALUE);
+        if (depth == this.MAX_DEPTH) {
+            mv.value = this.evaluator.evaluate(s);
+            return mv;
+        }
+        int[][] board = s.getBoard();
+        ArrayList<Position> moves = s.legalMoves();
+        for (Position move: moves) {
+            GameState newState = new GameState(board, s.getPlayerInTurn());
+            newState.insertToken(move);
+            MoveValue newMv = minValue(newState, depth+1);
+            if (newMv.value >= mv.value) {
+                mv.value = newMv.value;
+                mv.move = move;
+            }
+        }
+        return mv;
+    }
+
+
+    private MoveValue minValue(GameState s, int depth) {
+        MoveValue mv = new MoveValue(new Position(-1, -1), Integer.MAX_VALUE);
+        if (depth == this.MAX_DEPTH) {
+            mv.value = this.evaluator.evaluate(s);
+            return mv;
+        }
+        int[][] board = s.getBoard();
+        ArrayList<Position> moves = s.legalMoves();
+        for (Position move: moves) {
+            GameState newState = new GameState(board, s.getPlayerInTurn());
+            newState.insertToken(move);
+            MoveValue newMv = this.maxValue(newState, depth+1);
+            if (newMv.value <= mv.value) {
+                mv.value = newMv.value;
+                mv.move = move;
+            }
+        }
+        return mv;
+    }
 }
